95c981bc503a4fd4ad1a4f04c2e101216a416de2
Backendeng <backendeng0417@gmail.com>
2023-01-27 21:28:46 -0800

    feat: Adding enhancescroll

---
+++ Assets/_Game/_Scripts/Example/MyNGUIEnhanceItem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// NGUI Enhance item example
+/// </summary>
+public class MyNGUIEnhanceItem : EnhanceItem
+{
+    private Image mTexture;
+
+    protected override void OnAwake()
+    {
+        this.mTexture = GetComponent<Image>();
+        UIEventListener.Get(this.gameObject).onClick = OnClickNGUIItem;
+    }
+
+    private void OnClickNGUIItem(GameObject obj)
+    {
+        this.OnClickEnhanceItem();
+    }
+
+    // Set the item "depth" 2d or 3d
+    protected override void SetItemDepth(float depthCurveValue, int depthFactor, float itemCount)
+    {
+        if (mTexture.depth != (int)Mathf.Abs(depthCurveValue * depthFactor))
+            mTexture.depth = (int)Mathf.Abs(depthCurveValue * depthFactor);
+    }
+
+    // Item is centered
+    public override void SetSelectState(bool isCenter)
+    {
+        if (mTexture == null)
+            mTexture = this.GetComponent<Image>();
+        if (mTexture != null)
+            mTexture.color = isCenter ? Color.white : Color.gray;
+    }
+
+    protected override void OnClickEnhanceItem()
+    {
+        // item was clicked
+        base.OnClickEnhanceItem();
+    }
+}
