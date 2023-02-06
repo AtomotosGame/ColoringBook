dc45091614eb9ce0f25b283182bb23bf04197a0d
backendeng <backendeng0417@gmail.com>
2023-02-06 02:54:05 -0800

    Ading firebase config and testing it

--- Assets/_Game/_Scripts/_Main/ScrollListManagerColoring.cs
+++ Assets/_Game/_Scripts/_Main/ScrollListManagerColoring.cs
@@ -662,4 +662,47 @@ public class ScrollListManagerColoring : MonoBehaviour
         }
     }
 
+    public void GetFirebaseData () {
+        // Get a reference to the storage service, using the default Firebase App
+        // FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        // Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+        //     var dependencyStatus = task.Result;
+        //     if (dependencyStatus == Firebase.DependencyStatus.Available) {
+        //         // Create and hold a reference to your FirebaseApp,
+        //         // where app is a Firebase.FirebaseApp property of your application class.
+        //         Debug.Log(Firebase.FirebaseApp.DefaultInstance);
+        //         // app = Firebase.FirebaseApp.DefaultInstance;
+
+        //         // Set a flag here to indicate whether Firebase is ready to use by your app.
+        //     } else {
+        //         UnityEngine.Debug.LogError(System.String.Format(
+        //         "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+        //         // Firebase Unity SDK is not safe to use here.
+        //     }
+        // });
+        Debug.Log("a");
+        Firebase.Storage.StorageReference storageReference = Firebase.Storage.FirebaseStorage.DefaultInstance.GetReferenceFromUrl("gs://paintingproject-55bea.appspot.com");
+        Debug.Log("a");
+        storageReference.Child("/").GetBytesAsync(512*512).ContinueWith((System.Threading.Tasks.Task<byte[]> task) =>
+        {
+            Debug.Log("b");
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.Log("c");
+                Debug.Log(task.Exception.ToString());
+            }
+            else
+            {
+                Debug.Log("s");
+                byte[] fileContents = task.Result;
+                Texture2D texture = new Texture2D(1, 1);
+                texture.LoadImage(fileContents);
+                //if you need sprite for SpriteRenderer or Image
+                Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f,texture.width, 
+                texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+                Debug.Log("Finished downloading!");
+            }
+            Debug.Log("e");
+        });
+    }
 }
